21c7d21a0819a0ee5f7647af2ebe2037b0b76004
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-15 14:58:09 +0530

    first commit

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Dept { get; set; }
+        public double Salary { get; set; }
+        public DateTime Doj { get; set; }
+        public void AddEmployee(int id,string name,string dept, double salary,DateTime doj ) 
+        {
+        List<Employee> list = new List<Employee>() { new Employee() { Id=id,Name=name,Dept=dept, Salary=salary,Doj=doj}, };
+            foreach (Employee emp in list )
+            {
+                Console.WriteLine("{0,8} {1,8} {2,8} {3,8} {4,8}",emp.Id,emp.Name,emp.Dept,emp.Salary,emp.Doj);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment03;
+
+Console.WriteLine("Employee Details");
+Console.WriteLine("{0,8} {1,8} {2,8} {3,8} {4,8}", "ID", "Name", "Department", "Salary", "Date Of Joining");
+Employee employee = new Employee();
+employee.AddEmployee(1, "Naveen", "Testing", 32659, new DateTime(day:23,month:01,year:2024));
